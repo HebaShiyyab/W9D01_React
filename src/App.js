096c89e12846ef4c25f67c7659352cc7d91b3a73
Heba_Alshiyyab <81535776+HebaShiyyab@users.noreply.github.com>
2021-05-24 14:52:50 +0300

    Iam working

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import List from './components/list';
 const App = () => {
   const List = () => {
     const todo= [{todo: "wake up", id: 1}, {todo: "breakfast", id: 2}, {todo: "study", id: 3}]
+    const newTodo = todo.map((Element,i)=>{
+      return <ListItem  key = {i} todoList = {Element.todo} id = {Element.id}/>
+    })
     return <div><h1>Todo List </h1>
     <h1>ListItem </h1></div>;
   };
   return <>
   <h1>Todo List </h1>
-  <h1> List </h1>
-  <List todos = {todo}/>
+  <h1> todo </h1>
+  <list todo = {newTodo}/>
 
  </>;
 };
