bf7dc341954decaf6ea27d6f6a1ceba99571d451
Heba_Alshiyyab <81535776+HebaShiyyab@users.noreply.github.com>
2021-05-24 12:26:41 +0300

    Iam working

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ListItem from './components/list'
+import List from './components';
+
+
+// jsx
+const App = () => {
+  return <div><h1>Todo List </h1>
+ </div>;
+};
+
+export default App;
---
+++ src/components/list.js
@@ -0,0 +1,7 @@
+const List = () => {
+    const todo= [{todo: "wake up", id: 1}, {todo: "breakfast", id: 2}, {todo: "study", id: 3}]
+    return <div><h1>Todo List </h1>
+    <h1>ListItem </h1></div>;
+  };
+  
+  export default List;
